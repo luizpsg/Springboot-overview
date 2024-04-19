3eb23e4e46a29f9b00967992b76effa920813f4a
luizpsg <luiz.saud@sga.pucminas.br>
2024-04-18 23:29:37 -0300

    Add new endpoints for workout and fortune in FunRestController.java

--- src/main/java/com/luizpsg/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luizpsg/mycoolapp/rest/FunRestController.java
@@ -15,4 +15,18 @@ public class FunRestController {
   }
   
 
+  // expose a new endpoint for "workout"
+
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+      return "Run a hard 5k!";
+  }
+
+  // expose a new endpoint for "fortune"
+
+  @GetMapping("/fortune")
+  public String getDailyFortune() {
+      return "Today is your lucky day!";
+  }
+
 }
